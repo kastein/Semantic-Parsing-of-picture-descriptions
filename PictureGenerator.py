209af28da0c2f396e64c5631f8589dea1a8ba6f3
LeHarter <36102345+LeHarter@users.noreply.github.com>
2020-12-23 19:03:46 +0100

    Add files via upload

--- PictureGenerator.py
+++ PictureGenerator.py
@@ -2,36 +2,39 @@ import random
 from tkinter import *
 from PIL import Image, ImageDraw, ImageColor
 
-colours = ["red3","navy","green2",None]
-coordinates = {1:{1:(),2:(),3:(),4:()},2:{1:(),2:(),3:(),4:()},3:{1:(),2:(),3:(),4:()},4:{1:(),2:(),3:(),4:()}}
+
+
+red = "rgb(205,000,000)"
+blue = "rgb(000,000,139)"
+green = "rgb(102,205,000)"
+yellow = "rgb(220,220,000)"
+colours = ["yellow","red","blue","green",None]
+colour_dict = {"red":red,"blue":blue,"green":green,"yellow":yellow}
+coordinates = {1:{1:([(55,55),(95,95)]),2:([(105,55),(145,95)]),3:([(155,55),(195,95)]),4:([(205,55),(245,95)])},2:{1:([(55,105),(95,145)]),2:([(105,105),(145,145)]),3:([(155,105),(195,145)]),4:([(205,105),(245,145)])},3:{1:([(55,155),(95,195)]),2:([(105,155),(145,195)]),3:([(155,155),(195,195)]),4:([(205,155),(245,195)])},4:{1:([(55,205),(95,245)]),2:([(105,205),(145,245)]),3:([(155,205),(195,245)]),4:([(205,205),(245,245)])}}
 
 
 class Picture:
     def __init__(self):
-        row1 = random.choices(colours,weights=[1,1,1,8],k=4)
-        row2 = random.choices(colours,weights=[1,1,1,8],k=4)
-        row3 = random.choices(colours,weights=[1,1,1,8],k=4)
-        row4 = random.choices(colours,weights=[1,1,1,8],k=4)
-        self.red ="rgb(205,000,000)"
-        self.blue="rgb(000,000,139)"
-        self.green="rgb(102,205,000)"
+        row1 = random.choices(colours,weights=[2,2,2,2,25],k=4)
+        row2 = random.choices(colours,weights=[2,2,2,2,25],k=4)
+        row3 = random.choices(colours,weights=[2,2,2,2,25],k=4)
+        row4 = random.choices(colours,weights=[2,2,2,2,25],k=4)
         self.grid=[row1,row2,row3,row4]
         #self.canvas = Canvas(Tk(),width=1000, height=1000)
         
 
-    def draw_block(coordinates,colour):
-        pass
-
     def draw(self):
+        pass
         #self.canvas.pack()
         #self.canvas.create_rectangle(10,10,370,370,fill="white")
-        
-        image1 = Image.new("RGB", (600, 600), "white")
+        image1 = Image.new("RGB", (300, 300), "white")
         draw = ImageDraw.Draw(image1)
-        draw.rectangle([(100,100),(500,500)],fill="white",outline="black")
-        draw.rectangle([(50,50),(80,80)],fill=self.red,outline="black")
-        draw.rectangle([(90,90),(120,120)],fill=self.blue,outline="black")
-        draw.rectangle([(130,130),(160,160)],fill=self.green,outline="black")
+        draw.rectangle([(50,50),(250,250)],fill="white",outline="black")
+    
+        for row in coordinates:
+            for column in coordinates[row]:
+                if self.grid[row-1][column-1]:
+                    draw.rectangle(coordinates[row][column],fill=colour_dict[self.grid[row-1][column-1]],outline="black") 
         
         image1.save("test.jpg")
         import os
