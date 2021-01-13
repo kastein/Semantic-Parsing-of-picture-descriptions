e0b9b218b1a79a5d0e85f2b425b8c70e54c7458a
itsLuisa <lu.geiger3011@gmail.com>
2021-01-13 12:32:36 +0100

    somewhat closer to the actual goal

--- gui_simple.py
+++ gui_simple.py
@@ -1,17 +1,30 @@
 import PySimpleGUI as sg
+from BlockPictureGenerator import Picture
 import os.path
 from PIL import Image, ImageTk
 
-first_item = [
+start = [
     [
-        sg.Image("ProjectSketch.png"),  # try to read in jpg.files
-    ],
-    [
-        sg.Text("Describe the picture:"), # store what is being typed, when person hits enter
-        sg.In(size=(25, 1), enable_events=True, key="-INPUT-"),
-        sg.Button("Enter")
+        sg.Text("Hello! -Description-"),
+        sg.Button("Start Game", key="-START-")
     ]
 ]
+
+first_item = [
+    [
+        sg.Image(key="-IMAGE-")  # try to read in jpg.files
+    ],
+    [
+        #sg.Text("What is in the picture?", key="-INSTRUCTION-"),
+        sg.Button("show next picture", key="-NEXT-")
+    ],
+    [
+        sg.Text("Describe the picture:", key="-INSTRUCTION-"), # store what is being typed, when person hits enter
+        sg.In(size=(25, 1), enable_events=True, key="-INPUT-"),
+        sg.Button("Enter", key="-ENTER-")
+    ]
+]
+
 
 layout = [
     [
@@ -19,16 +32,37 @@ layout = [
     ]
 ]
 
-window = sg.Window("My Program", layout)
+starting_screen = sg.Window("Hello!", start)
+actualgame = sg.Window("My Program", layout)
+#window = starting_screen
+window = actualgame
 
 inpt = None
 while True:
     event, values = window.read()
+    print(event, values)
     if event == "Exit" or event == sg.WIN_CLOSED:
         break
+    # Beginning screen
+    #if event == "-START-":
+        #window.close()
+        #window = actualgame
+        #window.read()
+    # Displaying picture and taking input
+    if event == "-NEXT-":
+        picture = Picture(name="guitest").draw()
+        window["-IMAGE-"].update(filename="guitest.png")
     if event == "-INPUT-":
         inpt = values["-INPUT-"]
-    if event == "Enter":
+    if event == "-ENTER-":
         print(inpt)
+        window["-INSTRUCTION-"].update("Did you refer to this?")
+        picture = Picture(name="guitest").mark([(1,1)])
+        window["-IMAGE-"].update(filename="guitest_guess.png")
+        window["-INPUT-"].update("")
+        #window["-WELCOME-"].update("Thank you")
+        # try to say thank you to for the input
+        # feedback by the grammar
+
 
 window.close()
