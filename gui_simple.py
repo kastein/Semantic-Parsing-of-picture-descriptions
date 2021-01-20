e9b172ae0dda709f595b90a28681c02a20146b4d
itsLuisa <lu.geiger3011@gmail.com>
2021-01-20 17:31:21 +0100

    added lower() to input and linked input with grammar.py

--- gui_simple.py
+++ gui_simple.py
@@ -1,19 +1,20 @@
 import PySimpleGUI as sg
 from BlockPictureGenerator import Picture
-from grammar import Grammar, gold_lexicon, rules, functions
+from grammar import *
 import os.path
 from PIL import Image, ImageTk
+from PictureLevel import *
 
 gram = Grammar(gold_lexicon, rules, functions)
 
-'''start = [
+starting_screen = [
     [
-        sg.Text("Hello! -Description-"),
+        sg.Text("Hello! -Description- Press 'Start Game' to start the game"),
         sg.Button("Start Game", key="-START-")
     ]
-]'''
+]
 
-first_item = [
+game_screen = [
     [
         sg.Image(key="-IMAGE-")  # try to read in jpg.files
     ],
@@ -32,43 +33,70 @@ first_item = [
     ]
 ]
 
-
-layout = [
+layout_starting_screen = [
     [
-        sg.Column(first_item)
+        sg.Column(starting_screen)
     ]
 ]
 
-#starting_screen = sg.Window("Hello!", start)
-actualgame = sg.Window("My Program", layout)
-#window = starting_screen
-window = actualgame
+layout_game_screen = [
+    [
+        sg.Column(game_screen)
+    ]
+]
+
+start = sg.Window("Hello!", layout_starting_screen)
+actualgame = sg.Window("My Program", layout_game_screen)
+window = start
+#window = actualgame
 
 inpt = None
+
 while True:
     event, values = window.read()
     print(event, values)
+
     if event == "Exit" or event == sg.WIN_CLOSED:
         break
+
     # Beginning screen
-    #if event == "-START-":
-        #window["My Program"].update(actualgame)
-        #window = actualgame
-        #window.read()
+    if event == "-START-":
+        window.close()
+        window = actualgame
+        #event, values = window.read()
+        #picture = Picture(name="guitest").draw()
+        #window["-IMAGE-"].update(filename="guitest.png")
+        #continue
+
     # Displaying picture and taking input
     if event == "-NEXT-":
-        picture = Picture(name="guitest").draw()
-        window["-IMAGE-"].update(filename="guitest.png")
+        picture = setPicParameters(1, 1, "pictures")
+        picture.draw()
+        #picture = Picture(name="guitest").draw()
+        # change later
+        file_name = "pictures" + "_L" + str(1) + "_" + str(1) + ".png"
+        path_pict = "./" + "pictures" + "/" + file_name
+        window["-IMAGE-"].update(filename=path_pict)
         window["-INSTRUCTION-"].update("Describe the picture:")
+
     if event == "-INPUT-":
         inpt = values["-INPUT-"]
+
     if event == "-ENTER-":
+        inpt = inpt.lower()
         print(inpt)
         # here go into semantic parser
         lfs = gram.gen(inpt)
-        print(lfs)
+        for lf in lfs:
+            print("\tLF: {}".format(lf))
+            print('\tDenotation: {}'.format(gram.sem(lf)))
+        print(guessed_blocks)
+        guess = []
+        for b in guessed_blocks:
+            guess.append((b.y, b.x))
+        Picture(name="guitest").mark(guess)
         window["-INSTRUCTION-"].update("Did you refer to this?")
-        picture = Picture(name="guitest").mark([(1,1)])
+        #picture = Picture(name="guitest").mark([(1,1)])
         window["-IMAGE-"].update(filename="guitest_guess.png")
         window["-INPUT-"].update("")
     if event == "-YES-":
