b48416d3b2492a395c444a21f3b397e16b1981d2
LeHarter <36102345+LeHarter@users.noreply.github.com>
2021-02-19 10:49:00 +0100

    Add files via upload

--- back_and_forth.py
+++ back_and_forth.py
@@ -4,18 +4,20 @@ class BackAndForth_Iterator:
         self.index = -1
         self.list = somelist
 
-    def __iter__(self):
-        return self
-
-    def __next__(self):
+    def next(self):
+        if self.list == []:
+            raise StopIteration
         index += 1
         if index >= len(self.list):
             index = 0
         return self.list[index]
 
     def previous(self):
+        if self.list == []:
+            raise StopIteration
         index -= 1
         if index < 0:
             index = len(self.somelist)-1
         return self.list[index]
+    
         
